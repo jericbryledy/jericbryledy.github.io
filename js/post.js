aa3b239620787efac47bb067a171e418d735f26c
Jeric Bryle Sy Dy <jeric@jericbryledy.com>
2017-02-03 23:21:53 +0800

    for 22a0528eca3425bc5c83758bcee0d8afeb675520

--- js/post.js
+++ js/post.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	var postTimestamp = $("#post-timestamp");
 	if (postTimestamp.length === 1) {
 		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-		var postTimestampUtc = $("meta[itemprop=datePublished]").attr("content");
+		var postTimestampUtc = $("meta[name=datePublished]").attr("content");
 		var utcDate = new Date(postTimestampUtc);
 
 		postTimestamp.html(months[utcDate.getMonth()] + " " + utcDate.getDate() + ", " + utcDate.getFullYear());
